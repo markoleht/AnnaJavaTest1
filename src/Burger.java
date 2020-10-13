91f5b29c488481f33e8306fa8997686ec8545ed1
Marko Leht <marko.leht@khk.ee>
2020-10-14 00:49:48 +0300

    Burgeri ülesanne lahendamata

--- src/Burger.java
+++ src/Burger.java
@@ -1,49 +1,124 @@
-import java.util.*;
-//klass
+package sample;
+import java.util.Scanner;
+
 public class Burger {
+
     private String name;
     private String meatType;
     private String breadType;
     private double price;
     private String extras;
 
-
     //konstruktor
-    public Burger( String initialName) {
-        this.name = initialName;
-        this.meatType = "";
-        this.breadType = "";
-        this.price = 0;
+    public Burger( String name, String meatType, String breadType, int price) {
+        this.name = name;
+        this.meatType = meatType;
+        this.breadType = breadType;
+        this.price = price;
     }
-        public void setName(String newName) {
-            this.name = newName;
-        }
-
-        public void setMeatType(String newMeatType) {
-            this.meatType = newMeatType;
-        }
-
-        public void setBreadType(String newBreadType) {
-            this.breadType = "teraleib";
-        }
-
-        //lisandid
-
-        public static void main(String[] args) {
-            ArrayList<String> extras = new ArrayList<String>();
-
-            extras.add("Bacon");
-            extras.add("Peppers");
-            extras.add("Eggs");
-            extras.add("Pickles");
-        }
-
-            public void setPrice(int newPrice) {
-            this.price = newPrice;
-        }
-
-
-
-        // ...
+    public String name() {
+        return name;
     }
 
+    public String meatType() {
+        return meatType;
+    }
+
+    public String breadType() {
+        return breadType;
+    }
+
+    public double price() {
+        return price;
+    }
+
+    public class Order {
+
+        String[] burgers = {"Chicken burger", "Old Fashioned Cheeseburger", "New York Burger"};
+        String[] extras = {"Bacon", "Peppers", "Eggs", "Pickles"};
+        double[] burgerPrices = {6.50, 8.50, 9.50};
+        double[] extrasPrices = {2.00, 1.50, 1.00, 0.75};
+
+        public String[] getBurgers() {
+            return burgers;
+        }
+
+        public String[] getExtras() {
+            return extras;
+        }
+
+        public int getBurgersQuantity() {
+            return burgersQuantity;
+        }
+
+        public int getExtrasQuantity() {
+            return extrasQuantity;
+        }
+
+        public double[] getBurgerPrices() {
+            return burgerPrices;
+        }
+
+        public double[] getExtrasPrices() {
+            return extrasPrices;
+        }
+
+        public void setBurgers(String[] burgers) {
+            this.burgers = burgers;
+        }
+
+        public void setExtras(String[] extras) {
+            this.extras = extras;
+        }
+
+        public void setBurgerPrices(double[] burgerPrices) {
+            this.burgerPrices = burgerPrices;
+        }
+
+        public void setExtrasPrices(double[] extrasPrices) {
+            this.extrasPrices = extrasPrices;
+        }
+
+        public void setBurgersQuantity(int burgersQuantity) {
+            this.burgersQuantity = burgersQuantity;
+        }
+
+        public void setExtrasQuantity(int extrasQuantity) {
+            this.extrasQuantity = extrasQuantity;
+        }
+        // int burgersQuantity = 0;
+        //int extrasQuantity = 0;
+
+        //public void placeOrder( String burgers, String extras, double burgerPrices, double extrasPrices, int burgersQuantity, int extrasQuantity) {
+        //boolean orderPlaced = false;
+
+
+
+        double price = burgerPrices * burgersQuantity + extrasPrices * extrasQuantity ;
+            System.out.println("Sinu tellimuse " + burgersQuantity + " " + burgers + "lisandiga " + extrasQuantity + extras + " maksumus on: " + price + " EUR.");
+
+
+
+
+                    System.out.println();
+                    System.out.println("Chicken burger - kanalihaga, valge saiaga.  Lisandite valik koosneb juustust, salatist, tomatist, maioneesist. - Hind 6.50 EUR");
+                    System.out.println("Old Fashioned Cheeseburger - loomalihaga, valge saiaga.  Lisandite valik koosneb peekonist, kurgist, sibulast ja pekooni kastest. - Hind 8.50 EUR");
+                    System.out.println("New York Burger - sealihaga, teraleivaga. Lisandite valik koosneb ananassist, tomatist, kurgist ja sibulast. - Hind 9.50 EUR");
+                    System.out.println();
+                    System.out.println("    Bacon - 2.00 EUR");
+                    System.out.println("    Peppers - 1.50 EUR");
+                    System.out.println("    Eggs - 1.00 EUR");
+                    System.out.println("    Pickles - 0.70 EUR");
+
+                    System.out.println();
+        Scanner in = new Scanner(System.in);
+                    System.out.println("Millist burgerit soovid? ");
+        String burgers = in.nextLine();
+
+        Scanner in = new Scanner(System.in);
+                    System.out.print("Millist lisandit soovid? ");
+        String extras = in.nextLine();
+
+
+    }
+}
--- src/Burger/Main.java
+++ src/Burger/Main.java
@@ -1,22 +1,7 @@
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-        System.out.println( "Menüü - Vali omale meelepärane bugs");
-        System.out.println();
-        System.out.println("Chicken burger - kanalihaga, valge saiaga.  Lisandite valik koosneb juustust, salatist, tomatist, maioneesist. - Hind 6.50 EUR");
-        System.out.println("Old Fashioned Cheeseburger - loomalihaga, valge saiaga.  Lisandite valik koosneb peekonist, kurgist, sibulast ja pekooni kastest. - Hind 8.50 EUR");
-        System.out.println("New York Burger - sealihaga, teraleivaga. Lisandite valik koosneb ananassist, tomatist, kurgist ja sibulast. - Hind 9.50 EUR");
-        System.out.println();
-        System.out.println(" Bacon - 2.00 EUR");
-        System.out.println(" Peppers - 1.50 EUR");
-        System.out.println(" Eggs - 1.00 EUR");
-        System.out.println(" Pickles - 0.70 EUR");
 
-        String name = reader.nextLine();
-        System.out.println(name);
 
-}}
-
+    }
+}
