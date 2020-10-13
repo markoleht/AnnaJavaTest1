477f316a17a81a54112c56a4943962bc7d7dd909
Marko Leht <marko.leht@khk.ee>
2020-10-13 17:14:08 +0300

    Esimene ül tehtud

---
+++ src/TestOne.java
@@ -0,0 +1,19 @@
+
+//nested for loop + if
+
+public class TestOne {
+    public static void main(String[] args) {
+
+        for(int i = 9; i >= 0; i--) {
+            for(int j = 9; j >= 0; j--) {
+
+                if (j>i) {
+                    System.out.print(i + " ");
+                } else {
+                    System.out.print(j + " ");
+                }
+
+        }
+                    System.out.println();
+        }}}
+
